6f52cfe2500d98cd0639499db4c6d09d26f446cb
Ubuntu <ramyakrishnanmi@ramyakrishnanmi.lsatsvijufaungq2i0bvra52jf.gx.internal.cloudapp.net>
2020-12-18 16:04:21 +0000

    second commit

---
+++ src/main/java/com/java/docker_jenkins_integration_sample_project/Test.java
@@ -0,0 +1,11 @@
+package com.java.docker_jenkins_integration_sample_project;
+
+public class Test {
+
+	public static void main(String[] args)
+	
+	{
+		
+		System.out.println("Hello Jenkins!!");
+	}
+}
